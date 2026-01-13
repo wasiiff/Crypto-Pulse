59b4441f1d792575bcb1f51e5194a87d1665f6e7
Wasif Bin Nasir <162681224+wasiiff@users.noreply.github.com>
2026-01-13 14:29:42 +0500

    Favourite Api Routes Added

---
+++ app/api/favorites/route.ts
@@ -0,0 +1,54 @@
+import { authOptions } from "@/lib/auth";
+import { connectDB } from "@/lib/db";
+import { favoriteSchema } from "@/lib/validators/favorite";
+import Favorites from "@/models/Favorites";
+import { getServerSession } from "next-auth";
+
+export async function GET(req: Request) {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return Response.json({
+            error: "Unauthorized"
+        }, { status: 401 })
+    }
+
+    await connectDB();
+    const favorites = await Favorites.find({ UserId: session.user.id })
+    return Response.json(favorites);
+}
+
+export async function POST(req: Request) {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return Response.json({
+            error: "Unauthorized"
+        }, { status: 401 })
+    }
+    const body = req.json();
+    const data = favoriteSchema.parse(body);
+
+    await connectDB();
+
+    const addedFav = await Favorites.create({
+        UserId: session.user.id,
+        ...data,
+    })
+
+    return Response.json(addedFav);
+
+}
+
+export async function DELETE(req: Request) {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return Response.json({
+            error: "Unauthorized"
+        }, { status: 401 })
+    }
+
+    const { searchParams } = new URL(req.url);
+    const coinId = searchParams.get("coinId")
+
+    const deletedFav = await Favorites.findByIdAndDelete({ coinId });
+    return Response.json({ success: true });
+}

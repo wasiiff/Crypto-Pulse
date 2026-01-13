f4c68bcef41874fb28af23ed33b566d818bed59e
Wasif Bin Nasir <162681224+wasiiff@users.noreply.github.com>
2026-01-13 18:55:00 +0500

    React Querry Added

--- app/providers/providers.tsx
+++ app/providers/providers.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
+import { queryClient } from "@/lib/react-query";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </SessionProvider>
+  );
 }

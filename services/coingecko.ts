30776f9d3aa03ec89ff0b2115c85386cb903e06e
Wasif Bin Nasir <162681224+wasiiff@users.noreply.github.com>
2026-01-13 15:48:08 +0500

    API Services and Middleware added

---
+++ services/coingecko.ts
@@ -0,0 +1,41 @@
+const BASE_URL = process.env.COINGECKO_BASE_URL!;
+
+async function fetcher(url: string) {
+    const res = await fetch(url, {
+        headers: {
+            "Accept": "application/json",
+        },
+        next: { revalidate: 60 },
+    });
+
+    if (!res.ok) {
+        throw new Error("CoinGecko API error");
+    }
+
+    return res.json();
+}
+
+export async function getTrendingCoins() {
+    return fetcher(`${BASE_URL}/search/trending`);
+}
+
+export async function getMarketCoins({
+    vs_currency = "usd",
+    page = 1,
+    per_page = 20,
+}: {
+    vs_currency?: string;
+    page?: number;
+    per_page?: number;
+}) {
+    return fetcher(
+        `${BASE_URL}/coins/markets?vs_currency=${vs_currency}&order=market_cap_desc&per_page=${per_page}&page=${page}&sparkline=false`
+    );
+}
+
+
+export async function getCoinDetails(id: string) {
+    return fetcher(
+        `${BASE_URL}/coins/${id}?localization=false&tickers=false&market_data=true`
+    );
+}
